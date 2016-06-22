fa770a86bd20a54374abed883c3933bbd3dc3ce5
Andrii Perepeliak <aperepelyak@gmail.com>
2016-06-22 14:12:53 +0300

    div generate method

--- js_2.1/js/script.js
+++ js_2.1/js/script.js
@@ -4,8 +4,16 @@ var testQuiz = {
 		div.className = 'container';
 		document.body.appendChild(div);
 	},
+	generateDiv: function(parentElemClassName, newElemClassName) {
+		var div = document.createElement('div');
+		div.className = newElemClassName;
+		var parent = document.querySelector(parentElemClassName);
+		parent.appendChild(div);
+	}
 
 };
 
 testQuiz.generateContainer();
 console.log('container was created');
+testQuiz.generateDiv('.container', 'page-header');
+console.log('a new div was created');
