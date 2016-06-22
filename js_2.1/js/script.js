fc97a75b1a6c6e130fd97abfda6d2720acdf3300
Andrii Perepeliak <aperepelyak@gmail.com>
2016-06-22 13:55:06 +0300

    generate container method

--- js_2.1/js/script.js
+++ js_2.1/js/script.js
@@ -1,3 +1,11 @@
-var createQuiz = {
-	
+var testQuiz = {
+	generateContainer: function () {
+		var div = document.createElement('div');
+		div.className = 'container';
+		document.body.appendChild(div);
+	},
+
 };
+
+testQuiz.generateContainer();
+console.log('container was created');
