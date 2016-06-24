283d53731b91a855384eba13071af2f5152bba11
Andrii Perepeliak <aperepelyak@gmail.com>
2016-06-24 11:04:26 +0300

    solve problem with btns funcs

--- js_3.1_stopWatch/js/script.js
+++ js_3.1_stopWatch/js/script.js
@@ -1,45 +1,65 @@
 var k = 0;
+
 var milliseconds = document.getElementsByClassName('milisecs')[0];
 var seconds = document.getElementsByClassName('seconds')[0];
 var minutes = document.getElementsByClassName('minutes')[0];
 var hours = document.getElementsByClassName('hours')[0];
 
 var startButton = document.getElementsByClassName('btn-success')[0];
-function timer() {
-	k++;
+var clearButton = document.getElementsByClassName('btn-danger')[0];
 
-	if (!Math.floor(k / 10)) {
-		milliseconds.innerHTML = '0' + '0' + k;
-	} else if (!Math.floor(k / 100)) {
-		milliseconds.innerHTML = '0' + k;
-	} else {
-		milliseconds.innerHTML = k % 1000;
-	}
-
-
-
-	// seconds.innerHTML = k/1000;
-}
-
-var flag = true;
+var switchStartPause = true;
+var invokeClear = false;
 
 function startHandler() {
-	var msTimer = setInterval(timer, 1);
-	if (flag) {
-		startButton.className = 'btn btn-warning btn-sm';
-		startButton.innerHTML = 'Pause';
-		flag = false;
 
-	} else {
-		startButton.className = 'btn btn-success btn-sm';
-		startButton.innerHTML = 'Start';
+    function timer() {
 
-		clearInterval(msTimer);
-		flag = true;
-	}
+
+        if ((k % 1000) === 0) {
+            milliseconds.innerHTML = '000';
+        } else {
+            milliseconds.innerHTML = k % 1000;
+        }
+
+        if (switchStartPause) {
+            clearInterval(msTimer);
+        }
+
+        if (invokeClear) {
+            clearInterval(msTimer);
+            invokeClear = false;
+        }
+        k++;
+    }
+
+    var msTimer = setInterval(timer, 10);
+    if (switchStartPause) {
+        startButton.className = 'btn btn-warning btn-sm';
+        startButton.innerHTML = 'Pause';
+        switchStartPause = false;
+    } else {
+        clearInterval(msTimer);
+        startButton.className = 'btn btn-success btn-sm';
+        startButton.innerHTML = 'Start';
+        switchStartPause = true;
+    }
 }
 
- 
+function clearHandler() {
+    invokeClear = true;
+
+    k = 0;
+    milliseconds.innerHTML = '000';
+    seconds.innerHTML = '00';
+    minutes.innerHTML = '00';
+    hours.innerHTML = '00';
+
+    startButton.className = 'btn btn-success btn-sm';
+    startButton.innerHTML = 'Start';
+    switchStartPause = true;
+    invokeClear = false;
+}
 
 startButton.addEventListener('click', startHandler);
-
+clearButton.addEventListener('click', clearHandler);
