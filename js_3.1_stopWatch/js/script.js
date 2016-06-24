f49b782aa06a4ff589c185d01ee5b0d37ae71548
Andrii Perepeliak <aperepelyak@gmail.com>
2016-06-24 07:30:56 +0300

    change milliseconds spelling and function timer name

--- js_3.1_stopWatch/js/script.js
+++ js_3.1_stopWatch/js/script.js
@@ -1,19 +1,19 @@
 var k = 0;
-var miliseconds = document.getElementsByClassName('milisecs')[0];
+var milliseconds = document.getElementsByClassName('milisecs')[0];
 var seconds = document.getElementsByClassName('seconds')[0];
 var minutes = document.getElementsByClassName('minutes')[0];
 var hours = document.getElementsByClassName('hours')[0];
 
 var startButton = document.getElementsByClassName('btn-success')[0];
-function check() {
+function timer() {
 	k++;
 
 	if (!Math.floor(k / 10)) {
-		miliseconds.innerHTML = '0' + '0' + k;
+		milliseconds.innerHTML = '0' + '0' + k;
 	} else if (!Math.floor(k / 100)) {
-		miliseconds.innerHTML = '0' + k;
+		milliseconds.innerHTML = '0' + k;
 	} else {
-		miliseconds.innerHTML = k % 1000;
+		milliseconds.innerHTML = k % 1000;
 	}
 
 
@@ -24,7 +24,7 @@ function check() {
 var flag = true;
 
 function startHandler() {
-	var msTimer = setInterval(check, 1);
+	var msTimer = setInterval(timer, 1);
 	if (flag) {
 		startButton.className = 'btn btn-warning btn-sm';
 		startButton.innerHTML = 'Pause';
