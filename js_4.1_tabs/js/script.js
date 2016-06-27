e3e14153358e9edcc80376d69062cb29cb0e246a
Andrii Perepeliak <aperepelyak@gmail.com>
2016-06-27 18:46:45 +0300

    add tab switch

--- js_4.1_tabs/js/script.js
+++ js_4.1_tabs/js/script.js
@@ -1,3 +1,27 @@
 $(function () {
-	$('.wrapper').addClass('test');
+
+	// Tab-1 active
+	$('.link-1').on('click', function() {
+		$('.tab-1').css('display', 'block');
+		$('.tab-2').css('display', 'none');
+		$('.tab-3').css('display', 'none');
+		console.log('clicked tab 1');
+	});
+
+	// Tab-2 active
+	$('.link-2').on('click', function() {
+		$('.tab-1').css('display', 'none');
+		$('.tab-2').css('display', 'block');
+		$('.tab-3').css('display', 'none');
+		console.log('clicked tab 2');
+	});
+
+	// Tab-3 active
+	$('.link-3').on('click', function() {
+		$('.tab-1').css('display', 'none');
+		$('.tab-2').css('display', 'none');
+		$('.tab-3').css('display', 'block');
+		console.log('clicked tab 3');
+	});
+
 });
