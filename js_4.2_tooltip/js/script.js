9e634880623fc748051cb4222ea57753bab0e3ab
Andrii Perepeliak <aperepelyak@gmail.com>
2016-06-29 10:30:18 +0300

    add showHelp script and remove toggle

--- js_4.2_tooltip/js/script.js
+++ js_4.2_tooltip/js/script.js
@@ -2,26 +2,46 @@ $(function() {
 
     // tip 1
     $('.firstName').on('mouseover', function() {
-        $('.tip-1').slideToggle();
+        $('.tip-1').css('display', 'block');
+        $('.tip-2').css('display', 'none');
+        $('.tip-3').css('display', 'none');
     });
     $('.firstName').on('mouseout', function() {
-        $('.tip-1').slideToggle();
+        $('.tip-1').css('display', 'none');
     });
 
     // tip 2
     $('.lastName').on('mouseover', function() {
-        $('.tip-2').slideToggle();
+        $('.tip-2').css({
+            display: 'block',
+            marginTop: '49px'
+        });
+        $('.tip-1').css('display', 'none');
+        $('.tip-3').css('display', 'none');
     });
     $('.lastName').on('mouseout', function() {
-        $('.tip-2').slideToggle();
+        $('.tip-2').css('display', 'none');
     });
 
     // tip 3
     $('.address').on('mouseover', function() {
-		$('.tip-3').slideToggle();
-	});
-	$('.address').on('mouseout', function() {
-		$('.tip-3').slideToggle();
-	});
+        $('.tip-3').css({
+            display: 'block',
+            marginTop: '99px'
+        });
+        $('.tip-1').css('display', 'none');
+        $('.tip-2').css('display', 'none');
+    });
+    $('.address').on('mouseout', function() {
+        $('.tip-3').css('display', 'none');
+    });
+
+    // Show help
+    $('.showHelp').on('click', function() {
+        $('.tips').css({
+            display: 'block',
+            marginTop: '0'
+        });
+    });
 
 });
