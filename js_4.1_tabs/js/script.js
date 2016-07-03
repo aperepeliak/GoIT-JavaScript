14581354dc4d27701f8e754e8c56d4212fd580f4
Andrii Perepeliak <aperepelyak@gmail.com>
2016-07-03 18:43:55 +0300

    add fallback font, col-offset, vars in script

--- js_4.1_tabs/js/script.js
+++ js_4.1_tabs/js/script.js
@@ -1,33 +1,41 @@
 $(function () {
 
+	var $link1 = $('.link-1');
+	var $link2 = $('.link-2');
+	var $link3 = $('.link-3');
+
+	var $tab1 = $('.tab-1');
+	var $tab2 = $('.tab-2');
+	var $tab3 = $('.tab-3');
+
 	// Tab-1 active
-	$('.link-1').on('click', function() {
-		$('.tab-1').css('display', 'block');
-		$('.tab-2').css('display', 'none');
-		$('.tab-3').css('display', 'none');
+	$link1.on('click', function() {
+		$tab1.css('display', 'block');
+		$tab2.css('display', 'none');
+		$tab3.css('display', 'none');
 		$(this).parent().addClass('active');
-		$('.link-2').parent().removeClass('active');
-		$('.link-3').parent().removeClass('active');
+		$link2.parent().removeClass('active');
+		$link3.parent().removeClass('active');
 	});
 
 	// Tab-2 active
-	$('.link-2').on('click', function() {
-		$('.tab-1').css('display', 'none');
-		$('.tab-2').css('display', 'block');
-		$('.tab-3').css('display', 'none');
+	$link2.on('click', function() {
+		$tab1.css('display', 'none');
+		$tab2.css('display', 'block');
+		$tab3.css('display', 'none');
 		$(this).parent().addClass('active');
-		$('.link-1').parent().removeClass('active');
-		$('.link-3').parent().removeClass('active');
+		$link1.parent().removeClass('active');
+		$link3.parent().removeClass('active');
 	});
 
 	// Tab-3 active
-	$('.link-3').on('click', function() {
-		$('.tab-1').css('display', 'none');
-		$('.tab-2').css('display', 'none');
-		$('.tab-3').css('display', 'block');
+	$link3.on('click', function() {
+		$tab1.css('display', 'none');
+		$tab2.css('display', 'none');
+		$tab3.css('display', 'block');
 		$(this).parent().addClass('active');
-		$('.link-1').parent().removeClass('active');
-		$('.link-2').parent().removeClass('active');
+		$link1.parent().removeClass('active');
+		$link2.parent().removeClass('active');
 	});
 
 });
