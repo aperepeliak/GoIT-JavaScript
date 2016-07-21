b0085f5d65436aba831d3cc3f402e481e594b42b
aperepeliak <aperepelyak@gmai.com>
2016-07-21 22:41:02 +0300

    add glyphicon

--- js_8.1_ajax/js/script.js
+++ js_8.1_ajax/js/script.js
@@ -1,103 +1,39 @@
 $(function() {
 
-    // INITIAL SCRIPT
-    // var API_KEY = '2957253-77eda47a6d8c06c5cf8269032';
-    // var URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent('test');
-    // $.getJSON(URL, function(data) {
-    //     if (parseInt(data.totalHits) > 0)
-    //         $.each(data.hits, function(i, hit) { console.log(hit.pageURL); });
-    //     else
-    //         console.log('No hits');
-    // });
-
     var API_KEY = '2957253-77eda47a6d8c06c5cf8269032';
-    // var URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent('test');
-    // $.getJSON(URL, function(data) {
-    //     var ul = document.createElement("ul");
-    //     if (parseInt(data.totalHits) > 0) {
-    //         $.each(data.hits, function(i, hit) {
-    //             var li = document.createElement("li");
-    //             var inner = '<a href="' + hit.pageURL + '" target="_blank">  <img src="' + hit.previewURL + '"></a>';
-    //             li.innerHTML = inner;
-    //             ul.appendChild(li);
-    //             console.log('li', li);
-    //         });
-    //         $('body').html(ul);
-    //     } else
-    //         console.log('No hits');
-    // });
-
 
     $('.search-input').keypress(function(e) {
-    	var key = e.which;
-    	if (key === 13) {
-    		$('.test').click();
-    	}
-    });
-
-    $('.test').on('click', function () {
-
-    	var searchInput = $('.search-input').val();
-
-    	$.ajax({
-
-        url: "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(searchInput) + "&per_page=" + 42,
-        dataType: "jsonp",
-
-        success: function(data) {
-            // equal to previuos example
-            var ul = document.createElement("ul");
-            if (parseInt(data.totalHits) > 0) {
-                $.each(data.hits, function(i, hit) {
-                    var li = document.createElement("li");
-                    var inner = '<a href="' + hit.pageURL + '" target="_blank">  <img src="' + hit.previewURL + '"></a>';
-                    li.innerHTML = inner;
-                    ul.appendChild(li);
-                });
-                $('.search-results').html(ul);
-            } else {
-            	var message = $('<p>Sorry. No matches found :(</p>');
-            	$('.search-results').html(message);
-            } 
+        var key = e.which;
+        if (key === 13) {
+            $('.test').click();
         }
     });
 
+    $('.test').on('click', function() {
+
+        var searchInput = $('.search-input').val();
+        $.ajax({
+
+            url: "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(searchInput) + "&per_page=" + 42,
+            dataType: "jsonp",
+
+            success: function(data) {
+                var ul = document.createElement("ul");
+                if (parseInt(data.totalHits) > 0) {
+                    $.each(data.hits, function(i, hit) {
+                        var li = document.createElement("li");
+                        var inner = '<a href="' + hit.pageURL + '" target="_blank">  <img src="' + hit.previewURL + '"></a>';
+                        li.innerHTML = inner;
+                        ul.appendChild(li);
+                    });
+                    $('.search-results').html(ul);
+                } else {
+                    var message = $('<p>Sorry. No matches found :(</p>');
+                    $('.search-results').html(message);
+                }
+            }
+        });
+
     });
-
-    
-
-
-    // 	$.getJSON("http://search.yahooapis.com/WebSearchService/V1/webSearch?appid=YahooDemo&output=json&query=PHP&callback=?",
-    // function(data){
-    //     var ul = document.createElement("ul");
-    //     $.each(data.ResultSet.Result, function(i, val){
-    //            var li = document.createElement("li");
-    //             var inner = '<a href="'+val.Url+'" title="'+val.Url+'" target="_blank">'+val.Title+"</a>";
-    //             if (val.Summary != "" && val.Summary != "undefined") {
-    //                 inner += " - "+val.Summary;
-    //             }
-    //             li.innerHTML = inner;                               
-    //             ul.appendChild(li);
-    //     });
-    //     $('body').html(ul);
-    // });
-
-
-
-
-    // change key!!!
-    // $.getJSON("http://ajax.googleapis.com/ajax/services/search/web?v=1.0?key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=PHP&callback=GoogleCallback&context=?",
-    // function(data){
-    //     var ul = document.createElement("ul");
-    //     $.each(data.results, function(i, val){
-    //             var li = document.createElement("li");
-    //             li.innerHTML = '<a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+"</a> - "+val.content;                            
-    //             ul.appendChild(li);
-    //     });
-    //     $('body').html(ul);
-    // });
-
-
-
 
 });
