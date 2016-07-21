021eb2907f4a8786774bd3a0afd4d58b6b6e745b
aperepeliak <aperepelyak@gmai.com>
2016-07-21 21:19:18 +0300

    add enter press event

--- js_8.1_ajax/js/script.js
+++ js_8.1_ajax/js/script.js
@@ -27,13 +27,21 @@ $(function() {
     //         console.log('No hits');
     // });
 
+
+    $('.search-input').keypress(function(e) {
+    	var key = e.which;
+    	if (key === 13) {
+    		$('.test').click();
+    	}
+    });
+
     $('.test').on('click', function () {
 
     	var searchInput = $('.search-input').val();
 
     	$.ajax({
 
-        url: "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(searchInput),
+        url: "https://pixabay.com/api/?key=" + API_KEY + "&q=" + encodeURIComponent(searchInput) + "&per_page=" + 42,
         dataType: "jsonp",
 
         success: function(data) {
@@ -48,7 +56,7 @@ $(function() {
                 });
                 $('.search-results').html(ul);
             } else {
-            	var message = $('<p>Sorry :( No matches found.</p>');
+            	var message = $('<p>Sorry. No matches found :(</p>');
             	$('.search-results').html(message);
             } 
         }
