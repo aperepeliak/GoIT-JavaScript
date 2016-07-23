4a18f3146170122b2354acad6f3d74dc235733e6
aperepeliak <aperepelyak@gmai.com>
2016-07-23 20:53:52 +0300

    add browser-sync

--- js_9.1_gulp/gulpfile.js
+++ js_9.1_gulp/gulpfile.js
@@ -1,14 +1,29 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
 
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('app/css'));
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', ['browserSync', 'sass'], function () {
     gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/**/*.js', browserSync.reload);
+    
+});
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    });
 });
