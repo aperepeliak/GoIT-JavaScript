545579b1f5eede6aae91de20ae9413a04449a629
aperepeliak <aperepelyak@gmai.com>
2016-07-23 20:16:25 +0300

    add structure, gulp, sass, watch

---
+++ js_9.1_gulp/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+});
