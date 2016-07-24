0ccc5ae813cd4c06420f9c7cffdcf1248c1a6e07
aperepeliak <aperepelyak@gmai.com>
2016-07-24 16:36:23 +0300

    add images min plugin

--- js_9.1_gulp/gulpfile.js
+++ js_9.1_gulp/gulpfile.js
@@ -6,6 +6,16 @@ var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+        .pipe(cache(imagemin({
+            interlaced:true
+         })))
+         .pipe(gulp.dest('dist/images'));
+});
 
 gulp.task('useref', function () {
     return gulp.src('app/*.html')
