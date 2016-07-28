7f493ba39a869fcb87aba173d85286c318ba0bd8
aperepeliak <aperepelyak@gmai.com>
2016-07-28 12:24:59 +0300

    add social sprites

--- js_10.1_scss/gulpfile.js
+++ js_10.1_scss/gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('sass', function() {
 });
 
 gulp.task('watch', ['browserSync', 'sass'], function () {
-    gulp.watch('app/scss/main.scss', ['sass']);
+    gulp.watch('app/scss/**/*.scss', ['sass']);
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/**/*.js', browserSync.reload);
     
