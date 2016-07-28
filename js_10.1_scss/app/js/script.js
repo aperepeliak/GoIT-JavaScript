4cfa58d997f6129dd8f7af1213094555ffd6062c
aperepeliak <aperepelyak@gmai.com>
2016-07-28 09:10:02 +0300

    add basic html-structure

--- js_10.1_scss/app/js/script.js
+++ js_10.1_scss/app/js/script.js
@@ -12,21 +12,4 @@ $(function() {
         target: '+=1'
     });
 
-    $('.jcarousel-pagination').jcarouselPagination({
-        item: function(page) {
-            return '<a href="#' + page + '">' + page + '</a>';
-        }
-    });
-
-    // Selection
-    $(".chosen-select").chosen();
-
-    // Checkboxes
-    $('input').iCheck({
-        checkboxClass: 'icheckbox_square-yellow',
-        radioClass: 'iradio_square-yellow',
-        increaseArea: '20%' // optional
-    });
-
-    $('.css-checkbox').iCheck('destroy');
 });
