ad15a6fdfc1876c1cb2c8d4568343f3626d2ec9a
aperepeliak <aperepelyak@gmai.com>
2016-07-30 22:23:58 +0300

    working on carousel pagination

--- js_10.1_scss/app/js/script.js
+++ js_10.1_scss/app/js/script.js
@@ -1,15 +1,36 @@
-$(function() {
+$(function () {
 
-	// Carousel
+    // Carousel
     $('.jcarousel').jcarousel();
 
-
-    $('.jcarousel-prev').jcarouselControl({
-        target: '-=1'
+    $('.jcarousel-pagination').jcarouselPagination({
+        item: function (page) {
+            return '<a href="#' + page + '">' + '<div class="pages page-' + page + '" style="width:8px; height:8px; background-color:#000;"></div>' + '</a>';
+        }
     });
 
-    $('.jcarousel-next').jcarouselControl({
-        target: '+=1'
-    });
+    var $numPages = $('.pages')[0].addClass('active');
+
+    // console.log('number of pages = ', $numPages.length);
+
+    // console.log('$numPages[1] = ', $numPages[1]);
+
+    // $numPages.addClass("active");
+    
+
+    // function Paginate () {
+    //         for (var j = 0; j < $numPages.length; j++) {
+    //             if ($numPages[j].hasClass ("active")) {
+    //                 $numPages[j].removeClass("active");
+    //             }
+    //         }
+    //         $numPages[i].addClass("active");
+
+    //     }
+
+    // for (var i = 0; i < $numPages.length; i++) {
+    //     $numPages[0].on('click', Paginate);
+    // }
 
 });
+
