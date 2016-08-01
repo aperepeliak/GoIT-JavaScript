fa5c85213bd9930496550356d0a614e9b93bc6d7
aperepeliak <aperepelyak@gmai.com>
2016-08-01 13:56:15 +0300

    add config to prototypes

--- js_8.2_prototypes/js/script.js
+++ js_8.2_prototypes/js/script.js
@@ -1,15 +1,15 @@
-function Human(name, age, gender, height, weight) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-    this.height = height;
-    this.weight = weight;
+function Human(config) {
+    this.name = config.name;
+    this.age = config.age;
+    this.gender = config.gender;
+    this.height = config.height;
+    this.weight = config.weight;
 }
 
-function Worker(name, age, gender, height, weight, workPlace, salary) {
-    Human.apply(this, arguments);
-    this.workPlace = workPlace;
-    this.salary = salary;
+function Worker(config) {
+    Human.apply(this, config);
+    this.workPlace = config.workPlace;
+    this.salary = config.salary;
 }
 
 Worker.prototype = Object.create(Human.prototype);
@@ -19,10 +19,10 @@ Worker.prototype.work = function() {
     console.log(this.name + ' is working');
 };
 
-function Student(name, age, gender, height, weight, studyPlace, allowance) {
-    Human.apply(this, arguments);
-    this.studyPlace = studyPlace;
-    this.allowance = allowance;
+function Student(config) {
+    Human.apply(this, config);
+    this.studyPlace = config.studyPlace;
+    this.allowance = config.allowance;
 }
 
 Student.prototype = Object.create(Human.prototype);
@@ -32,15 +32,35 @@ Student.prototype.watchTVseries = function() {
     console.log(this.name + ' is watching tv series');
 };
 
-var engineer = new Worker('John', 32, 'male', 180, 80, 'Google', 2500);
-var sales = new Worker('Chris', 25, 'male', 167, 75, 'Microsoft', 1700);
-var bachelor = new Student('Anna', 21, 'female', 175, 68, 'KPI', 150);
-var master = new Student('Julia', 22, 'female', 185, 72, 'HNURE', 170);
+var john = {
+    name: 'John',
+    age: 32,
+    gender: 'male',
+    height: 180,
+    weight: 80,
+    workPlace: 'Google',
+    salary: 2500
+};
+
+var chris = {
+    name: 'Chris',
+    age: 25,
+    gender: 'male',
+    height: 167,
+    weight: 75,
+    workPlace: 'Microsoft',
+    salary: 1700
+};
+
+var engineer = new Worker(john);
+var sales = new Worker(chris);
+// var bachelor = new Student('Anna', 21, 'female', 175, 68, 'KPI', 150);
+// var master = new Student('Julia', 22, 'female', 185, 72, 'HNURE', 170);
 
 console.log('engineer = ', engineer);
 console.log('sales = ', sales);
-console.log('bachelor = ', bachelor);
-console.log('master = ', master);
+// console.log('bachelor = ', bachelor);
+// console.log('master = ', master);
 
 engineer.work();
-master.watchTVseries();
+// master.watchTVseries();
