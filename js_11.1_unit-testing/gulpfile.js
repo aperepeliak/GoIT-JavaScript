b0405f838b73ce1ced74f944b580ae661e2ce44c
aperepeliak <aperepelyak@gmail.com>
2016-08-02 16:57:35 +0300

    init project 11.1

---
+++ js_11.1_unit-testing/gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+var runSequence = require('run-sequence');
+var jasmine = require('gulp-jasmine');
+
+gulp.task('watch', ['browserSync'], function () {
+    gulp.watch('css/style.css');
+    gulp.watch('index.html', browserSync.reload);
+    gulp.watch('js/*.js', browserSync.reload);
+    
+});
+
+gulp.task('jasmine', () =>
+    gulp.src('js/spec.js')
+        // gulp-jasmine works on filepaths so you can't have any plugins before it 
+        .pipe(jasmine())
+);
+
+gulp.task('default', function(callback) {
+    runSequence(['browserSync', 'watch'], callback);
+});
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+    });
+});
---
+++ js_11.1_unit-testing/js/toLocalStorage.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var numQuestions = 3;
+var numAnswers = 3;
+var questions = new Array(numQuestions);
+
+var answers = new Array(numQuestions);
+for (var i = 0; i < numQuestions; i++) {
+    answers[i] = new Array(numAnswers);
+}
+
+questions[0] = { "question": "Какой из этих языков был создан не Аннерсом Хейлсбергом?" };
+questions[1] = { "question": "Кто является автором языка C++?" };
+questions[2] = { "question": "Автором JavaScript является:" };
+
+answers[0][0] = { "answer": "Delphi" };
+answers[0][1] = { "answer": "Haskell" };
+answers[0][2] = { "answer": "C#" };
+
+answers[1][0] = { "answer": "Бобби Джонс" };
+answers[1][1] = { "answer": "Билл Джой" };
+answers[1][2] = { "answer": "Бьёрн Страуструп" };
+
+answers[2][0] = { "answer": "Брендан Эйх" };
+answers[2][1] = { "answer": "Гвидо ван Россум" };
+answers[2][2] = { "answer": "Павел Дуров" };
+
+for (var i = 0; i < numQuestions; i++) {
+    localStorage.setItem('q' + (i + 1), JSON.stringify(questions[i]));
+    for (var j = 0; j < numAnswers; j++) {
+        localStorage.setItem('a' + (i + 1) + '.' + (j + 1), JSON.stringify(answers[i][j]));
+    }
+}
