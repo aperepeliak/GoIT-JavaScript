f70f3d0ca30163ca1ccdb69b8dba68190252887c
aperepeliak <aperepelyak@gmail.com>
2016-08-03 13:51:21 +0300

    add tests

--- js_11.1_unit-testing/spec/spec.js
+++ js_11.1_unit-testing/spec/spec.js
@@ -1,5 +1,40 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+var toLocalStorage = require('../js/toLocalStorage.js');
+
+describe("toLocalStorage", function() {
+  it("generates answers list", function() {
+
+    var result;
+    result = toLocalStorage.createAnswersList();
+    expect(result).toEqual('created answers list');
   });
+
+   it("addQuestions()", function() {
+
+    var result;
+    result = toLocalStorage.addQuestions();
+    expect(result).toEqual('questions added');
+  });
+
+  it("check number of questions", function() {
+
+    var result;
+    result = toLocalStorage.numQuestions;
+    expect(result).toBe(3);
+  });
+
+  it("check questions array", function() {
+    expect(toLocalStorage.questions).toBeDefined();
+  });
+
+  it("check answers array", function() {
+    expect(toLocalStorage.answers).toBeDefined();
+  });
+
+
+// возвращает ошибку, потому что localStorage не определен. Наверное, это из-за использования phantomjs?
+  // it("generates test quiz", function() {
+  //   var result;
+  //   result = toLocalStorage.generate();
+  //   expect(result).toEqual('testQuiz succesfully created');
+  // });
 });
