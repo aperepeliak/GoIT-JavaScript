5c59a5b4ed762ebbb0d8e91a11ba7aa98914403c
aperepeliak <aperepelyak@gmail.com>
2016-08-07 12:56:11 +0300

    add pages, plugin works bad

--- js_10.1_scss/app/js/main.js
+++ js_10.1_scss/app/js/main.js
@@ -9,15 +9,51 @@ $(function() {
 
     $('.jcarousel-pagination').jcarouselPagination({
         item: function (page) {
-            return '<a href="#' + page + '">' + '<span class="pages page-' + page + '" style="display:block; width:8px; height:8px; background-color:#000;"></span>' + '</a>';
+            return '<a class="pgs" href="#' + page + '">' + '<span class="pages page-' + page + '" style="display:block; width:8px; height:8px; background-color:#000;"></span>' + '</a>';
         }
     });
-    
+    $pgs = $('.pgs');
     $pages = $('.pages');
-    console.log('$pages = ', $pages);
-    console.log('$pages[0] = ', $pages[0]);
+    // console.log('$pages = ', $pages);
+    // console.log('$pages[0] = ', $pages[0]);
+    // console.log('$pages.length = ', $pages.length);
+  
+    
 
-    $pages.eq(0).addClass('test');
+    $pages.eq(0).addClass('active');
+
+    $pgs.eq(0).on('click', function() {
+        console.log('test');
+        $pages.removeClass('active');
+        $pages.eq(0).addClass('active');
+    });
+
+    $pgs.eq(1).on('click', function() {
+        console.log('test');
+        $pages.removeClass('active');
+        $pages.eq(1).addClass('active');
+    });
+
+    $pgs.eq(2).on('click', function() {
+        console.log('test');
+        $pages.removeClass('active');
+        $pages.eq(2).addClass('active');
+    });
+
+   
+
+    // for (var i = 0; i < $pgs.length; i++) {
+    //     $pgs.eq(i).on('click', MakeItActive);
+    // }
+
+    //  function MakeItActive () {
+    //     console.log('test = ', i);
+    //     $pages.eq(i).addClass('active');
+    // }
+
+   
+
+
 
     // $items = $('.items');
     //  console.log('$items = ', $items);
