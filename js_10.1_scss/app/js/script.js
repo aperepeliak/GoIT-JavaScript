b9f5ced5af25e0263d88874ee491d5805a985920
aperepeliak <aperepelyak@gmail.com>
2016-08-10 14:33:35 +0300

    fix pagination

--- js_10.1_scss/app/js/script.js
+++ js_10.1_scss/app/js/script.js
@@ -1,44 +1,26 @@
 $(function () {
 
-    // Carousel
     $('.jcarousel').jcarousel();
 
     $( "#accordion" ).accordion();
-
-    $('.jcarousel-pagination')
-    .on('.jcarouselpagination:active', 'a', function() {
-        $(this).addClass('active');
-        console.log('test');
-    })
-    .on('.jcarouselpagination:inactive', 'a', function() {
-        $(this).removeClass('active');
-    })    
-    .jcarouselPagination({
+   
+    $('.jcarousel-pagination').jcarouselPagination({
         item: function (page) {
             return '<a class="pgs" href="#' + page + '">' + '<span class="pages page-' + page + '" style="display:block; width:8px; height:8px; background-color:#000;"></span>' + '</a>';
         }
     });
-    // $pgs = $('.pgs');
-    // $pages = $('.pages');
 
-    // $pages.eq(0).addClass('active');
+    $('.jcarousel-pagination')
+    .on('jcarouselpagination:active', 'a', function() {
+        $(this).addClass('active');
+    })
+    .on('jcarouselpagination:inactive', 'a', function() {
+        $(this).removeClass('active');
+    });
 
-    // $pgs.eq(0).on('click', function () {
-    //     $pages.removeClass('active');
-    //     $pages.eq(0).addClass('active');
-    // });
-
-    // $pgs.eq(1).on('click', function () {
-    //     $pages.removeClass('active');
-    //     $pages.eq(1).addClass('active');
-    // });
-
-    // $pgs.eq(2).on('click', function () {
-    //     $pages.removeClass('active');
-    //     $pages.eq(2).addClass('active');
-    // });
+    $pgs = $('.pgs');
+    $pgs.eq(0).addClass('active');
 
     $('.jcarousel > ul').css({ "top": "0", "left": "0" });
-
 });
 
