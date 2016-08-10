48694655ed81063d48f2185af790a26df15d6fcf
aperepeliak <aperepelyak@gmail.com>
2016-08-10 13:49:19 +0300

    change pagination code

--- js_10.1_scss/app/js/script.js
+++ js_10.1_scss/app/js/script.js
@@ -5,30 +5,38 @@ $(function () {
 
     $( "#accordion" ).accordion();
 
-    $('.jcarousel-pagination').jcarouselPagination({
+    $('.jcarousel-pagination')
+    .on('.jcarouselpagination:active', 'a', function() {
+        $(this).addClass('active');
+        console.log('test');
+    })
+    .on('.jcarouselpagination:inactive', 'a', function() {
+        $(this).removeClass('active');
+    })    
+    .jcarouselPagination({
         item: function (page) {
             return '<a class="pgs" href="#' + page + '">' + '<span class="pages page-' + page + '" style="display:block; width:8px; height:8px; background-color:#000;"></span>' + '</a>';
         }
     });
-    $pgs = $('.pgs');
-    $pages = $('.pages');
+    // $pgs = $('.pgs');
+    // $pages = $('.pages');
 
-    $pages.eq(0).addClass('active');
+    // $pages.eq(0).addClass('active');
 
-    $pgs.eq(0).on('click', function () {
-        $pages.removeClass('active');
-        $pages.eq(0).addClass('active');
-    });
+    // $pgs.eq(0).on('click', function () {
+    //     $pages.removeClass('active');
+    //     $pages.eq(0).addClass('active');
+    // });
 
-    $pgs.eq(1).on('click', function () {
-        $pages.removeClass('active');
-        $pages.eq(1).addClass('active');
-    });
+    // $pgs.eq(1).on('click', function () {
+    //     $pages.removeClass('active');
+    //     $pages.eq(1).addClass('active');
+    // });
 
-    $pgs.eq(2).on('click', function () {
-        $pages.removeClass('active');
-        $pages.eq(2).addClass('active');
-    });
+    // $pgs.eq(2).on('click', function () {
+    //     $pages.removeClass('active');
+    //     $pages.eq(2).addClass('active');
+    // });
 
     $('.jcarousel > ul').css({ "top": "0", "left": "0" });
 
