75807d4a9ff779984835a451deaa16400cc49b0b
aperepeliak <aperepelyak@gmail.com>
2016-08-26 11:14:19 +0300

    add masonry layout

--- js_13_final/app/js/main.js
+++ js_13_final/app/js/main.js
@@ -12,13 +12,32 @@ $(function () {
 
     $('.jcarousel').jcarousel();
 
-     $('.jcarousel-prev').jcarouselControl({
+    $('.jcarousel-prev').jcarouselControl({
         target: '-=1'
     });
 
     $('.jcarousel-next').jcarouselControl({
         target: '+=1'
     });
-    
+
+
+    // $.ajax({
+    //     url: "api.pixplorer.co.uk/image?amount=8",
+    //     dataType: "jsonp",
+    //     success: function (data) {
+    //         var items = $('.grid-item');
+    //         if(data.count > 0) {
+
+    //         }
+    //     }
+    // });
+
+
+    $('.grid').masonry({
+        // options
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer'
+
+    });
 });
 
