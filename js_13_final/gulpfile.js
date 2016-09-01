8125c65a969c2473cf14f616e894987837f4b7e1
aperepeliak <aperepelyak@gmail.com>
2016-09-01 11:24:07 +0300

    change gulpfile

--- js_13_final/gulpfile.js
+++ js_13_final/gulpfile.js
@@ -12,78 +12,65 @@ var concat = require('gulp-concat');
 var cache = require('gulp-cache');
 
 gulp.task('js', function () {
-    gulp.src([
-        'app/js/*.js', '!app/js/main.js'
-    ])
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('app/js'));
+  gulp.src(['app/js/*.js'])
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('pages', function () {
-    return gulp.src('app/*.html')
-        .pipe(gulp.dest('dist'));
+  return gulp.src('app/*.html')
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean:dist', function () {
-    return del.sync('dist');
+  return del.sync('dist');
 });
 
 gulp.task('images', function () {
-    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
-        .pipe(cache(imagemin({
-            progressive: true,
-            interlaced: true
-        })))
-        .pipe(gulp.dest('dist/images'));
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    .pipe(cache(imagemin({
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('sass', function () {
-    return gulp.src('app/scss/main.scss')
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+  return gulp.src('app/scss/main.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 gulp.task('browserSync', function () {
-    browserSync.init({
-        server: {
-            baseDir: 'app'
-        },
-    });
+  browserSync.init({
+    server: {
+      baseDir: 'dist'
+    },
+  });
 });
 
-gulp.task('watch', ['browserSync', 'sass', 'js'], function () {
-    gulp.watch('app/scss/**/*.scss', ['sass']);
-    gulp.watch('app/js/*.js', ['js']);
-    gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/main.js', browserSync.reload);
+gulp.task('watch', ['browserSync', 'build'], function () {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/js/*.js', ['js']);
+  gulp.watch('app/*.html', ['pages']);
+  gulp.watch('dist/*.html', browserSync.reload);
+  gulp.watch('dist/js/main.js', browserSync.reload);
+  gulp.watch('dist/css/main.css', browserSync.reload);
 });
 
-gulp.task('default', function (callback) {
-    runSequence(['sass', 'js', 'browserSync', 'watch'], callback);
-});
-
-gulp.task('uglify', function () {
-    return gulp.src('app/js/main.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
-
-gulp.task('cssnano', function () {
-    return gulp.src('app/css/main.css')
-        .pipe(cssnano())
-        .pipe(gulp.dest('dist/css'));
-});
-
+gulp.task('default', ['watch']);
 
 gulp.task('build', function (callback) {
-    runSequence('clean:dist',
-        ['cssnano', 'uglify', 'images', 'pages'],
-        callback);
+  runSequence('clean:dist',
+    ['sass', 'js', 'images', 'pages'],
+    callback);
 });
